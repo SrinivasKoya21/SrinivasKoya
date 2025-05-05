c9b9591b90f5816d413875da09f2af96c774d270
SrinivasKoya21 <saisrinivaskoya21@gmail.com>
2025-05-04 22:43:16 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({ 
   plugins: [react(), tailwindcss()],
   server: { port: 3000 },
-  base:'/Srinivas/'
+  base:'/SrinivasKoya/'
 })
 
